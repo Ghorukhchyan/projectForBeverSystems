c2306f7e27e63c98dfcbff374fb209e63d519b36
Armen Ghorukhchyan <Armenghorukhchyan@gmail.com>
2021-06-25 17:55:16 +0400

    firstCommit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <div class="post" v-for="(post, i) in posts" :key="i">
+      <img v-bind:src="post.picture.large" />
+      <p>{{ post.name.title }} {{ post.name.first }} {{ post.name.last }}</p>
+      <p>Country ` {{ post.location.country }}</p>
+      <p>Email ` {{ post.email }}</p>
+      <button @click="refreshPeople()">Refresh List</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      posts: [],
+    };
+  },
+
+  async mounted() {
+    axios
+      .get("https://randomuser.me/api/?results=")
+      .then((val) => (this.posts = val.data.results));
+  },
+
+  methods: {
+    refreshPeople() {
+      //   location.reload();
+      axios
+        .get("https://randomuser.me/api/?results=")
+        .then((val) => (this.posts = val.data.results));
+    },
   },
 };
 </script>
+
+<style scoped>
+.post {
+  background-color: aqua;
+  text-align: center;
+  width: 400px;
+  margin: 0 auto;
+  margin-top: 5px;
+}
+
+img {
+  border-radius: 50%;
+}
+
+button {
+  margin: 0 auto;
+  color: blue;
+  border-radius: 8px;
+}
+</style>
